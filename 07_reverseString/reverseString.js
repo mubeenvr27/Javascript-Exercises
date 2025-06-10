3914d622b23efc79e73d0b919fca89c4efd3ff96
mubeenvr27 <mubeenvr27@gmail.com>
2025-06-10 13:44:21 +0500

    q6 to 11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Check if the input is a string
+    if (typeof string !== 'string') {
+        return 'Input must be a string';
+    }
+    
+    // Split the string into an array of characters, reverse it, and join it back into a string
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
